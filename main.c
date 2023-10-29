5d5bd8293bcde148a385474c2b092954a5c73792
Spacerulerwill <williamdredding@gmail.com>
2023-10-29 16:45:02 +0000

    Reformatting and more sysexits

--- main.c
+++ main.c
@@ -17,17 +17,29 @@ const SourceFile readSourceFile(const char* filePath) {
     char *buffer;
     
     fp = fopen (filePath , "rb");
-    if( !fp ) perror(filePath),exit(1);
 
-    fseek( fp , 0L , SEEK_END);
-    lSize = ftell( fp );
-    rewind( fp );
+    if(!fp) {
+        perror(filePath);
+        exit(EX_IOERR);
+    }
 
-    buffer = calloc( 1, lSize+1 );
-    if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+    fseek(fp,0L,SEEK_END);
+    lSize = ftell(fp);
+    rewind(fp);
 
-    if( 1!=fread( buffer , lSize, 1 , fp) )
-    fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+    buffer = calloc(1, lSize+1);
+    if(!buffer) {
+        fclose(fp);
+        fputs("memory alloc fails",stderr);
+        exit(EX_IOERR);
+    }
+
+    if(fread( buffer , lSize, 1 , fp) != 1) {
+        close(fp);
+        free(buffer);
+        fputs("entire read fails",stderr);
+        exit(EX_IOERR);
+    }
     fclose(fp);
 
     SourceFile sourceFile = {
